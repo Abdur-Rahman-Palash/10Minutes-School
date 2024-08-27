5e4d6f946b978bff0c97d86d17bb38150ca5fb3d
palash0111 <aymanislammayan@gmail.com>
2024-08-28 04:46:32 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        boxShadow: {
+          'custom-black': '0 4px 8px rgba(0, 0, 0, 0.5)', // Example custom black shadow
+        },
+      },
+    },
+    plugins: [],
+  }
+  
